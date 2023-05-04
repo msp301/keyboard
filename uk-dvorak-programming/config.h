2596ad494a492b2eafd289da95d994de5aceb88a
Martin Pritchard <1196474+msp301@users.noreply.github.com>
2023-05-04 21:30:00 +0100

    Fixed some keycodes to produce expected UK keys

--- uk-dvorak-programming/config.h
+++ uk-dvorak-programming/config.h
@@ -3,9 +3,9 @@
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
 #define ORYX_CONFIGURATOR
-#define USB_SUSPEND_WAKEUP_DELAY 0
 #define IGNORE_MOD_TAP_INTERRUPT
-#define FIRMWARE_VERSION u8"ZePxo/5xG4B"
+#define USB_SUSPEND_WAKEUP_DELAY 0
+#define FIRMWARE_VERSION u8"ZePxo/7Gg9J"
 #define RAW_USAGE_PAGE 0xFF60
 #define RAW_USAGE_ID 0x61
 #define LAYER_STATE_8BIT
