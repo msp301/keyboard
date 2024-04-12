ed2546c7a2a08536c3bdd730c6471525de806234
Martin Pritchard <1196474+msp301@users.noreply.github.com>
2024-04-12 22:04:28 +0100

    Build and tidy up after

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 
 SCRIPT=$(readlink -f $0)
 REPO_DIR="$(dirname $SCRIPT)"
+
+BUILD_DIR="${REPO_DIR}/build"
 KEYBOARDS_DIR="${REPO_DIR}/keyboards"
 QMK_DIR="${REPO_DIR}/qmk_firmware"
 QMK_KEYBOARDS_DIR="${QMK_DIR}/keyboards"
@@ -10,11 +12,15 @@ SRC_DIRS=( $(fd -t f "." "$KEYBOARDS_DIR" | xargs -I{} dirname {} | sort -u | xa
 
 git submodule update --init --recursive
 
-for dir in "${SRC_DIRS[@]}"; do
-    echo "Building ${dir} ${QMK_KEYBOARDS_DIR}/${dir}/${USER}"
-    ln -sf "${KEYBOARDS_DIR}/${dir}" "${QMK_KEYBOARDS_DIR}/${dir}/keymaps/${USER}"
-done
+for keyboard in "${SRC_DIRS[@]}"; do
+    KEYMAP_DIR="${KEYBOARDS_DIR}/${keyboard}"
+    QMK_KEYMAP_DIR="${QMK_KEYBOARDS_DIR}/${keyboard}/keymaps/${USER}"
 
-for dir in "${SRC_DIRS[@]}"; do
-    make BUILD_DIR="${REPO_DIR}/build" -C "${QMK_DIR}" "${dir}:${USER}"
+    echo "Building ${KEYMAP_DIR} => ${QMK_KEYMAP_DIR}"
+
+    ln -sf "${KEYMAP_DIR}" "${QMK_KEYMAP_DIR}"
+
+    make BUILD_DIR="${BUILD_DIR}" -C "${QMK_DIR}" "${keyboard}:${USER}"
+
+    rm -f "${QMK_KEYMAP_DIR}"
 done
