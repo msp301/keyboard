c83f46d02bff726175ba1940cb6e53d71a928473
Martin Pritchard <1196474+msp301@users.noreply.github.com>
2024-04-14 11:39:56 +0100

    Fix typo in default Q4 keymap

--- keyboards/keychron/q4/iso/keymap.c
+++ keyboards/keychron/q4/iso/keymap.c
@@ -63,4 +63,4 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______,  _______,                            _______,                            _______,  _______,  _______,  _______)
 };
 
-// clnag-format on
+// clang-format on
