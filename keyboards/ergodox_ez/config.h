16fec8e6bbd2524af6280c8e6bbe328566872f1a
Martin Pritchard <1196474+msp301@users.noreply.github.com>
2024-04-19 23:34:06 +0100

    Start "UK Dvorak Mini" - combining ideas from Miryoku
    Trying out a more minimal keyboard layout on the Ergodox.

--- keyboards/ergodox_ez/config.h
+++ keyboards/ergodox_ez/config.h
@@ -3,8 +3,10 @@
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
 #define ORYX_CONFIGURATOR
+#define QUICK_TAP_TERM 0
+
 #define USB_SUSPEND_WAKEUP_DELAY 0
-#define FIRMWARE_VERSION u8"PlyXq/mdzO3"
+#define FIRMWARE_VERSION u8"xmAxx/VpLwj"
 #define RAW_USAGE_PAGE 0xFF60
 #define RAW_USAGE_ID 0x61
 #define LAYER_STATE_8BIT
