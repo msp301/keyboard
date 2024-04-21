d3bbf81bb0a752ba60c4df18451403b8a15a4174
Martin Pritchard <1196474+msp301@users.noreply.github.com>
2024-04-21 10:54:57 +0100

    Mirror symbol layer toggle on thumb keys

--- keyboards/ergodox_ez/keymap.c
+++ keyboards/ergodox_ez/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRAVE,       KC_NUBS,        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                                                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
                                                                                                     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
                                                                                                                     KC_TRANSPARENT, KC_TRANSPARENT,
-                                                                                    LT(1,KC_BSPC),  KC_TAB,         KC_TRANSPARENT, KC_TRANSPARENT, KC_ENTER,       LT(3,KC_SPACE)
+                                                                                    LT(1,KC_BSPC),  LT(3,KC_TAB),   KC_TRANSPARENT, KC_TRANSPARENT, LT(3,KC_ENTER), KC_SPACE
   ),
   [1] = LAYOUT_ergodox_pretty(
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
